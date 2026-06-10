b9c2b778a2bea7c33f3e329cc329dfd9f11d3d14
sohehh <gks0304hsh@naver.com>
2026-06-10 15:18:32 +0900

    first commit

---
+++ js/swiper-action.js
@@ -0,0 +1,61 @@
+
+const projectSwiper = new Swiper(".js-project-swiper", {
+    slidesPerView: 1,
+    grid: {
+        rows: 2,
+        // fill: 'row',
+    },
+    spaceBetween: 0,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".content--project .swiper-button-group .swiper-button-next",
+        prevEl: ".content--project .swiper-button-group .swiper-button-prev",
+    },
+    breakpoints : {
+        // 브라우저가 768보다 클 때
+        768 : {
+            slidesPerView: 2,
+        },
+        // 브라우저가 1024보다 클 때
+        1024 : {
+            slidesPerView: 3,
+        }
+    }
+});
+
+const projectAppSwiper = new Swiper(".js-projectApp-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    autoHeight : true,
+    navigation: {
+        nextEl: ".content--projectApp .swiper-button-group .swiper-button-next",
+        prevEl: ".content--projectApp .swiper-button-group .swiper-button-prev",
+    },
+});
+
+const projectWebSwiper = new Swiper(".js-projectWeb-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    autoHeight : true,
+    navigation: {
+        nextEl: ".content--projectWeb .swiper-button-group .swiper-button-next",
+        prevEl: ".content--projectWeb .swiper-button-group .swiper-button-prev",
+    },
+});
+
+const projectScreenSwiper = new Swiper(".js-projectScreen-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    autoHeight : true,
+    navigation: {
+        nextEl: ".content--projectScreen .swiper-button-group .swiper-button-next",
+        prevEl: ".content--projectScreen .swiper-button-group .swiper-button-prev",
+    },
+});
+// DOM이 다 그려진 후 강제로 update : 모바일에서 마지막요소 잘림 해결
+// window.addEventListener('load', () => {
+//     tabs.update();
+// });
